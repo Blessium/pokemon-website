fd2ee081c9cfe7eef8462bbca25febfff642d22e
astolfo <psnstefano12@gmail.com>
2023-04-03 20:03:29 +0200

    added select and return json

--- backend/main.py
+++ backend/main.py
@@ -18,12 +18,17 @@ except mariadb.Error as e:
 
 app = Flask(__name__)
 
-@app.route("/<pokemon>")
-def getPokemon(pokemon):
-    query = 'SELECT p.* AS pokemon FROM pokemon p where p.identifier="' + request.view_args['pokemon'] + '";'
+@app.route("/pokemon", methods=['GET'])
+def getPokemon():
+    args = request.args
+    pokemon = args.get('id')
+
+    query = 'SELECT p.id, p.identifier, p.height, p.weight, g.identifier FROM pokemon p \
+                JOIN pokemon_species ps ON ps.id = p.species_id \
+                    JOIN generations g ON g.id = ps.generation_id \
+                            WHERE p.identifier LIKE"' + pokemon + '%";'
     mycursor.execute(query)
-    res = mycursor.fetchall()
-    json_data = []
-    for result in res:
-        json_data.append(result)
-    return jsonify(result = json_data)
+
+    res = [dict((mycursor.description[i][0], value) for i, value in enumerate(row)) for row in mycursor.fetchall()] 
+
+    return jsonify(result = res)
